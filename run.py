fc4d8992d65d6b9aecbc3c9389a3d70a765b16e6
MrX <savsher@gmail.com>
2017-12-29 06:28:58 +0300

    Fri 29 Dec 06:28:58 MSK 2017

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import queue
 
 DOMINO_SET = [(i, j) for i in range(7) for j in range(7) if j >= i]
+CONFIG = []
 
 class PlayTable(object):
     def __init__(self):
@@ -14,7 +16,16 @@ class Bazar(object):
         pass
 
 class Player(object):
-    def __init__(self):
+    def __init__(self, type ):
+        self._bones = list()
+        self._players = list()
+        self._draw = list()
+    def move(self):
+        pass
+    def get_bone(self):
+        pass
+    def add_player(self, pl):
+
         pass
 
 def main():
@@ -22,11 +33,22 @@ def main():
     read configuration file
     :return:
     """
-    m = PlayTable()
-    while True:
-        break
+    p1 = Player('M')
+    p2 = Player('M')
+    p3 = Player('M')
+    p4 = Player('H')
 
-    print('TEST')
+    m = PlayTable()
+
+    q = queue.Queue()
+    for i in range(4):
+        q.put(i)
+    while not q.empty():
+        z = q.get()
+        print(z)
+        if z == x3:
+            break
+        q.put(z)
 
 if __name__  == '__main__':
     main()
